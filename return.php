860cb9805098f34271e347e658d8cf92d2812e25
samuel dias <93612090+SDias14@users.noreply.github.com>
2023-03-09 13:12:43 -0300

    delete finalizado

--- return.php
+++ return.php
@@ -3,6 +3,7 @@
 header("Access-Control-Allow-Origin: *"); // permite que qualquer site acesse a api
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS'); // permite que qualquer metodo seja usado (get, post, put, delete, etc)
 
+
 header('Content-Type: application/json'); // diz que o conteudo retornado é um json
 
 
